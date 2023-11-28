1b8dce1a2139b8717f08d8ef7f9bd423f1d27551
jaumecf <caf@paucasesnovescifp.cat>
2023-11-28 18:36:10 +0100

    Home using User info from Provider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,10 @@ import 'package:provider/provider.dart';
 import '../providers/login_provider.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  final TextStyle textStyle = TextStyle(
+      fontSize: 32.0, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold);
 
   @override
   Widget build(BuildContext context) {
@@ -25,24 +28,22 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                missatge,
-                style: TextStyle(
-                  fontSize: 32.0,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          )
-        ],
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              missatge,
+              style: textStyle,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              loginProvider.user!.uid,
+              style: textStyle,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
